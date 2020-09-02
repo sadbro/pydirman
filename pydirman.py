f301a176bf0446ede92639ad3f301f3b287090a0
sadbro.linux <68669684+sadbro@users.noreply.github.com>
2020-09-03 00:08:11 +0530

    Update pydirman.py
    Added new features.

--- pydirman.py
+++ pydirman.py
@@ -36,7 +36,7 @@ def __display():
 
     print(colored("\nPYTHON DIRECTORY MANAGEMENT\n", "red", attrs=['underline', 'bold']))
     print(colored("Directory", "yellow") +" | "+ colored("File", "white"))
-    print("===========================")
+    print("=====================================================================")
     counter = 0
     for index,FI_FO in enumerate(LS_DIR):
         if os.path.isfile(FI_FO):
@@ -46,11 +46,14 @@ def __display():
             print("[{}] ".format(index) + colored("{}/".format(FI_FO), "yellow")) #color for directory
             counter += 1
 
-    print("===========================")
+    print("=====================================================================")
     print("TOTAL FILES/DIRECTORIES: "+ colored("{}".format(counter), "red", attrs=['bold']))
-    print("YOUR CURRENT LOCATION: "+ colored("{}".format(CUR_DIR), "red", attrs=['bold']))
-    print("===========================")
-    print("[options] = [c]hoose/[e]xit/[p]revious/[s]earch ")
+    print("YOUR CURRENT LOCATION: "+ colored("{}".format(CUR_PATH), "red", attrs=['bold']))
+    print("=====================================================================")
+    print("[walker]  = "+ colored("[c]", 'red') +"hoose-" +colored("[p]", 'red') +"revious-"+ colored("[s]", 'red') +"earch")
+    print("[utility] = "+ colored("[m]", 'red') +"odern-"+ colored("[t]", 'red') +"erminal-" +colored("[e]", 'red') +"xit")
+    print("[writer]  = "+ colored("[n]", 'red') +"ewfile-" +colored("[d]", 'red') +"eletefile")
+
 
 def __chdir(CUR_DIR):
 
@@ -59,57 +62,62 @@ def __chdir(CUR_DIR):
     SEARCH_DIR = []
     for item in LS_DIR:
         SEARCH_DIR.append(str(item.lower()))
+        
+    __all__ = ['choose', 'exit', 'previous', 'search', 'terminal', 'newfile', 'deletefile', 'modern']
 
-    CUR_PATH = os.path.abspath(CUR_DIR)
-    __all__ = ['choose', 'exit', 'previous', 'search']
-
-    com = str(input(colored("\npydirman", "blue", attrs=['underline']) +colored(">", "blue")))
+    com = str(input(colored("\npydirman", "blue", attrs=['underline']) +colored(">", "blue"))).strip()
     if com.lower() == "c":
 
-        ask = input("Enter your index: ")
-        if ask.isdigit():
-            ask = int(ask)
+        try: 
+            ask = input("Enter your index: ")
+            if ask.isdigit():
+                ask = int(ask)
 
-            try:
-                if os.path.isdir(str(LS_DIR[ask])):
-                    os.chdir(str(LS_DIR[ask]))
-                    __display()
-                    __chdir(LS_DIR[ask])
-
-                elif os.path.isfile(str(LS_DIR[ask])):
-                    porr = str(input(colored("THIS IS A FILE! [p]rint/[r]emain/[e]dit:", "white", attrs=['underline']) +" "))
-                    CUR_FILE = str(LS_DIR[ask])
-
-                    if porr.lower() == "p":
-                        print(colored("\n" +"="*56 +"START"+ "="*56 +"\n", "red"))
-                        os.system("cat " +CUR_FILE)
-                        print(colored("\n" +"="*57 +"END"+ "="*57+ "\n", "red"))
-                        stat = str(input("[c]ontinue/[e]xit: "))
-
-                        if stat.lower() == "c":
-                            __display()
-                            __chdir(CUR_DIR)
-
-                        elif stat.lower() == "e":
-                            print(colored("See you soon!", "white"))
-                            sys.exit(0)
-
-                    elif porr.lower() == "r":
+                try:
+                    if os.path.isdir(str(LS_DIR[ask])):
+                        os.chdir(str(LS_DIR[ask]))
                         __display()
-                        __chdir(os.getcwd())
+                        __chdir(LS_DIR[ask])
+
+                    elif os.path.isfile(str(LS_DIR[ask])):
+                        porr = str(input(colored("THIS IS A FILE! [p]rint/[r]emain/[e]dit:", "white", attrs=['underline']) +" "))
+                        CUR_FILE = str(LS_DIR[ask])
+
+                        if porr.lower() == "p":
+                            print(colored("\n" +"="*56 +"START"+ "="*56 +"\n", "red"))
+                            os.system("cat " +CUR_FILE)
+                            print(colored("\n" +"="*57 +"END"+ "="*57+ "\n", "red"))
+                            stat = str(input("[c]ontinue/[e]xit: "))
+
+                            if stat.lower() == "c":
+                                __display()
+                                __chdir(CUR_DIR)
+
+                            elif stat.lower() == "e":
+                                print(colored("See you soon!", "white"))
+                                sys.exit(0)
+
+                        elif porr.lower() == "r":
+                            __display()
+                            __chdir(os.getcwd())
                         
-                    elif porr.lower() == "e":
-                        os.system("sudo gedit {}".format(CUR_FILE))
-                        __chdir(os.getcwd())
-
-            except PermissionError:
-                cprint("\nThis directory is off limits!! Are you root?? I don't think so. BACK OFF!\n".upper(), "red", attrs=['bold'])
-                sleep(2)
+                        elif porr.lower() == "e":
+                            os.system("sudo gedit {} >/dev/null 2>&1".format(CUR_FILE))
+                            __chdir(os.getcwd())
+                
+                except IndexError:
+                   print("oops, you entered a wrong number.")
+                   __chdir(os.getcwd())
+                    
+            elif ask.isalpha():
+                print("Please enter index of the file :[")
+                print("[HINT] if you can't find the index then you can find it by typing [s] ;]")
                 __chdir(os.getcwd())
+  
 
-        elif ask.isalpha():
-            print("Please enter index of the file :[")
-            print("[HINT] if you can't find the index then you can find it by typing [s] ;]")
+        except PermissionError:
+            cprint("\nThis directory is off limits!! Are you root?? I don't think so. BACK OFF!\n".upper(), "red", attrs=['bold'])
+            sleep(2) 
             __chdir(os.getcwd())
 
     elif com.lower() == "e":
@@ -127,18 +135,69 @@ def __chdir(CUR_DIR):
             __chdir(CUR_DIR)
 
     elif com.lower() == "s":
-        search = str(input("\nEnter your search: ")).lower()
+        search = str(input("\nEnter your search: "))
+        #cacheDir = []
+        #for obj in LS_DIR:
+        #    if search in obj:
+        #        cacheDir.append(obj)
+                
+        search = search.lower()
         if search in SEARCH_DIR:
             print("File/Folder found. " +colored("\nindex: [{}]".format(SEARCH_DIR.index(search)), "red", attrs=['bold']))
             if os.path.isfile(search):
                 cprint("is a [FILE]", "red", attrs=['bold'])
             elif os.path.isdir(search):
                 cprint("is a [DIRECTORY]", "red", attrs=['bold'])
-
+        #    print(cacheDir)
+            
+        #elif len(cacheDir) > 1:
+        #    print("Multiple instances found! Showing all")
+        #    for inst in cacheDir:
+        #        print("[{}] {}".format(SEARCH_DIR.index(inst), inst))
+                
         else:
             print("Sorry. your results returned no results. maybe you didn't enter the correct name.")
         __chdir(CUR_DIR)
-
+        
+    elif com.lower() == "t":
+        termDir = os.getcwd()
+        os.system("qterminal -d -w {} >/dev/null 2>&1".format(termDir))
+        __chdir(os.getcwd())
+        
+    elif com.lower() == "n":
+        filename = str(input("Enter file name [create]: "))
+        os.system("sudo gedit {}".format(filename))
+        __chdir(os.getcwd())
+        
+    elif com.lower() == "d":
+        print("=====================================================================")
+        counter = 0
+        for index,FI_FO in enumerate(LS_DIR):
+            if os.path.isfile(FI_FO):
+                print("[{}] ".format(index) + colored("{}".format(FI_FO), "white")) #color for file instance
+                counter += 1
+            
+            #elif os.path.isdir(FI_FO):
+                #print("[{}] ".format(index) + colored("{}/".format(FI_FO), "yellow")) #color for directory
+                #counter += 1
+        print("=====================================================================")
+        delfile = str(input("Enter file name [delete]: "))
+        os.system("sudo rm {}".format(delfile))
+        __chdir(os.getcwd())
+        
+    elif com.lower() == "m":
+        print("=====================================================================")
+        counter = 0
+        for index,FI_FO in enumerate(sorted(os.listdir(os.getcwd()))):
+            if os.path.isfile(FI_FO):
+                print("[{}] ".format(index) + colored("{}".format(FI_FO), "white")) #color for file instance
+                counter += 1
+            elif os.path.isdir(FI_FO):
+                print("[{}] ".format(index) + colored("{}/".format(FI_FO), "yellow")) #color for directory
+                counter += 1
+        print("=====================================================================")
+        __chdir(os.getcwd())
+        
     elif com.lower() in __all__:
         print("\nplease enter the the first letter [{}] only".format(com.lower()[0]))
         __chdir(os.getcwd())
@@ -146,6 +205,6 @@ def __chdir(CUR_DIR):
     else:
        print("\nEnter a valid command!")
        __chdir(os.getcwd())
-
+       
 __display()
 __chdir(CUR_DIR)
