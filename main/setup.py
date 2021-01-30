0d10b737168573112d3289857bd0bd00a3be0f11
sadbro.linux <68669684+sadbro@users.noreply.github.com>
2021-01-31 00:48:45 +0530

    added java and js support

--- main/setup.py
+++ main/setup.py
@@ -5,7 +5,7 @@ import os
 os.chdir('.')
 
 require = ['termcolor', 'pip']
-os_require = ['qterminal', 'gcc', 'g++']
+os_require = ['qterminal', 'gcc', 'g++', 'nodejs', 'default-jdk']
 PATH = os.path.realpath('.')
 docs = sorted(os.listdir(PATH))
 
