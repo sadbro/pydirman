9b4e0023f39c6a398a776e336e4cac425690d01c
sahil_sagwekar <sahilss2652@gmail.com>
2022-12-14 20:01:02 +0530

    fixed some errors

--- main/arch_setup.py
+++ main/arch_setup.py
@@ -20,14 +20,13 @@ elif 'debian' in os_details.lower():
 else:
     print(colored("Unsupported GNU/Linux Distro", "red", attrs=['underlined', 'bold']))
     sys.exit(-1)
-#docs = sorted(os.listdir(PATH))
 
 for package in require:
     if package in sys.modules:
         pass
     else:
         if OS_VERSION == 'arch':
-            os.system('sudo pacman -S python3-{} --noconfirm'.format(package))
+            os.system('sudo pacman -S python-{} --noconfirm'.format(package))
         elif OS_VERSION == 'debian':
             os.system('sudo apt install python3-{}'.format(package))
 
@@ -39,13 +38,13 @@ if OS_VERSION == 'arch':
         result = sp.getoutput('pacman -Qe {}'.format(package))
 
     # PACKAGE MENU
-    for i in len(os_require):
+    for i in range(len(os_require)):
         print(colored('[{}]: {}'.format(i, os_require[i]), "yellow"))
     req_packages = input(colored(f"\nEnter the index of the packages you want to install\ne.g. 0 1 2\n", "red", attrs=['bold']))
 
     print("Installing {} packages".format(len(req_packages)))
     for index in req_packages.strip().split():
-        os.system('sudo pacman -S {} --noconfirm'.format(os_require[index]))
+        os.system('sudo pacman -S {} --noconfirm'.format(os_require[int(index)]))
 
 elif OS_VERSION == 'debian':
     for os_package in os_require:
@@ -55,6 +54,7 @@ def fixer(cmd, ind):
     '''sets the file permission for cmd file to ind'''
     os.system('sudo chmod {} {}'.format(ind, cmd))
 
+#docs = sorted(os.listdir(PATH))
 #for doc in docs:
 #    fixer(doc, 777)
 
